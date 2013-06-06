a717d9f61ecea91e85ab8af3115da87c4bc3bdbd
kimspindel <kimspindel@gmail.com>
2013-06-07 01:17:26 +0200

    copied particles.h and particles.cpp

--- src/ghosthunt.cpp
+++ src/ghosthunt.cpp
@@ -1,1 +1,26 @@
 #include "ghosthunt.h"
+#include "particlestate.h"
+#include "splashstate.h"
+
+void Particles::setup()
+{
+    window.create(windbreeze::VideoMode(1366, 768), "Particles");
+    window.setFramerateLimit(60);
+
+    stateMachine.addGameState("splash", std::shared_ptr<SplashState>(new SplashState(sfWindow, inputHandler)));
+    stateMachine.addGameState("particles", std::shared_ptr<ParticleState>(new ParticleState(sfWindow, inputHandler, actionHandler)));
+    stateMachine.setCurrentState("splash");
+}
+
+void Particles::destroy()
+{
+    window.close();
+}
+
+void Particles::loop()
+{
+    stateMachine.run();
+
+    if(stateMachine.isFinished())
+        quit();
+}
