7a80ab23299d8d7c48621ecbdd9d02a7b0a7209d
kimspindel <kimspindel@gmail.com>
2013-06-20 17:00:09 +0200

    errors everywhere - shall try integrating sfgui it into the game anyway

--- src/sfgui.h
+++ src/sfgui.h
@@ -1,16 +1,38 @@
 #include <SFGUI/SFGUI.hpp>
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
 
 class Clicky
 {
     public:
+        void run();
         void menu();
         void join();
         void host();
 
+        //menu
+        void onJoinButtClick();
+        void onHostButtClick();
+        void onInstructButtClick();
+        void onOptionsButtClick();
+
+        //join
+        void onConnectButtClick();
+        void onBackButtClick();
+
+        //host
+
     private:
         sfg::SFGUI sfgui;
-        sfg::Box::Ptr box(sfg::Box::Create(sfg::Box::VERTICAL, 26.0f));
+        sf::RenderWindow renwin(sf::VideoMode(800, 600), "Hejsan!")
+        sfg::Box::Ptr menuBox(sfg::Box::Create(sfg::Box::VERTICAL, 26.0f));
+        sfg::Box::Ptr joinBox(sfg::Box::Create(sfg::Box::VERTICAL, 26.0f));
+        sfg::Box::Ptr hostBox(sfg::Box::Create(sfg::Box::VERTICAL, 26.0f));
+        sfg::Desktop desktop;
+        sf::Event event;
+        sf::Clock clock;
+        std::string state = "menu";
 
         //menu
         sfg::Button::Ptr joinButt(sfg::Button::Create("JOIN GAME"));
@@ -30,4 +52,74 @@ class Clicky
         sfg::Button::Ptr hostBackButt(sfg::Button::Create("Back"));
 };
 
+void Clicky::run()
+{
 
+    if (state == "menu")
+        menu();
+    else if (state == "join")
+        join();
+
+    renwin.resetGLStates();
+    while (renwin.isOpen())
+    {
+        while (renwin.pollEvent(event))
+        {
+            desktop.HandleEvent(event);
+            if (event.type == sf::Event::Closed)
+            {   
+                renwin.close();
+            }   
+        }
+
+        desktop.Update(clock.restart().asSeconds());
+
+        renwin.clear();
+        sfgui.Display(renwin);
+        renwin.display();
+    } 
+}
+    
+void Clicky::menu()
+{
+    joinButt(sfg::Widget::OnLeftClick).Connect(&Clicky::onJoinButtClick, this);
+    hostButt(sfg::Widget::OnLeftClick).Connect(&Clicky::onHostButtClick, this);
+    instructButt(sfg::Widget::OnLeftClick).Connect(&Clicky::onInstructButtClick, this);
+    optionsButt(sfg::Widget::OnLeftClick).Connect(&Clicky::onOptionsButtClick, this);
+
+    menuBox->Pack(joinButt, false);
+    menuBox->Pack(hostButt, false);
+    menuBox->Pack(instructButt, false);
+    menuBox->Pack(optionsButt, false);
+    desktop.Add(menuBox);
+}
+
+void Clicky::join()
+{}
+
+//menu
+void Clicky::onJoinButtClick()
+{
+    state = "join";
+}
+
+void Clicky::onHostButtClick()
+{
+    state = "host";
+}
+
+/* void onInstructButtClick();
+{
+    state = "instructions";
+}
+
+void onOptionsButtClick();
+{
+    state = "options";
+}   */
+
+int main()
+{
+    Clicky clicky;
+    clicky.run();
+}
