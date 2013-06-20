2cdc5671d38ad8a3c293e0e82eb7f9c49d43e9a9
Tobias Widlund <widlundtobias@gmail.com>
2013-06-20 23:48:37 +0200

    fixi

--- src/sfgui.h
+++ src/sfgui.h
@@ -1,6 +1,7 @@
 #pragma once
-#include <SFGUI/SFGUI.hpp>
 #include <SFML/Graphics.hpp>
+#include <SFGUI/SFGUI.hpp>
+#include <SFGUI/Box.hpp>
 #include <iostream>
 
 class Clicky
@@ -25,26 +26,26 @@ class Clicky
 
     private:
         sfg::SFGUI sfgui;
-        sfg::Box::Ptr menuBox(sfg::Box::Create(sfg::Box::VERTICAL, 26.0f));
-        sfg::Box::Ptr joinBox(sfg::Box::Create(sfg::Box::VERTICAL, 26.0f));
-        sfg::Box::Ptr hostBox(sfg::Box::Create(sfg::Box::VERTICAL, 26.0f));
+        sfg::Box::Ptr menuBox = sfg::Box::Create(sfg::Box::VERTICAL, 26.0f);
+        sfg::Box::Ptr joinBox = sfg::Box::Create(sfg::Box::VERTICAL, 26.0f);
+        sfg::Box::Ptr hostBox = sfg::Box::Create(sfg::Box::VERTICAL, 26.0f);
         sfg::Desktop desktop;
         std::string state = "menu";
 
         //menu
-        sfg::Button::Ptr joinButt(sfg::Button::Create("JOIN GAME"));
-        sfg::Button::Ptr hostButt(sfg::Button::Create("HOST GAME"));
+        sfg::Button::Ptr joinButt = sfg::Button::Create("JOIN GAME");
+        sfg::Button::Ptr hostButt = sfg::Button::Create("HOST GAME");
         //sfg::Button::Ptr instructButt(sfg::Button::Create("Instructions"));
         //sfg::Button::Ptr optionsButt(sfg::Button::Create("Options"));
 
         //join
         sfg::Label::Ptr joinLabel = sfg::Label::Create("Join a game");
         sfg::Label::Ptr joinIPLabel = sfg::Label::Create("IP:");
-        sfg::Button::Ptr joinConnectButt(sfg::Button::Create("JOIN GAME"));
-        sfg::Button::Ptr joinBackButt(sfg::Button::Create("Back"));
+        sfg::Button::Ptr joinConnectButt = sfg::Button::Create("JOIN GAME");
+        sfg::Button::Ptr joinBackButt = sfg::Button::Create("Back");
 
         //host
         sfg::Label::Ptr hostWaitLabel = sfg::Label::Create("Waiting for the other player to connect");
         sfg::Label::Ptr hostIPLabel = sfg::Label::Create("Your IP:");
-        sfg::Button::Ptr hostBackButt(sfg::Button::Create("Back"));
+        sfg::Button::Ptr hostBackButt = sfg::Button::Create("Back");
 };
